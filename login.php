2b09e81d4cb1c2fab08a2a860d1a5ddd44d9d9b4
Zahkung Mai <113776489+zahkungnangmunmai@users.noreply.github.com>
2022-10-16 19:43:53 +0630

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<center>
+<div>
+	<label>username</label>
+	<input type="username" name="required">
+</div>
+
+<div>
+	<label>password</label>
+	<input type="password" name="required">
+</div>
+
+<div>
+	<input type="submit" value="Login">
+</div>
+
+
+
+
+</center>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,48 @@
+//change navbar styles on scroll
+
+window.addEventListener('scroll',()=> {
+document.querySelector('nav').classList.toggle
+('window-scroll', window.scrollY>0)
+})
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open');
+
+        //change icon = faq.querySelector('.faq_icon i');
+
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        } else{
+            icon.className === 'uil uil-plus';
+        }
+    })
+    })
+
+
+
+    //show/hide nav menu
+    const menu = document.querySelector(".nav_menu");
+    const menuBtn = document.querySelector("#open-menu-btn");
+    const CloseBtn = document.querySelector("#close-menu-btn");
+
+
+    menuBtn.addEventListener('click',() => {
+        menu.style.display = "flex";
+        CloseBtn.style.display = "inline-block";
+        menuBtn.style.display = "none";
+    })
+
+
+    //close nav menu
+    const closeNav = () => {
+        menu.style.display = "none";
+        CloseBtn.style.display = "none";
+        menuBtn.style.display = "inline-block";
+    }
+
+    CloseBtn.addEventListener('click',closeNav)
